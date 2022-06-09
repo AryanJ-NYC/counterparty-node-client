938334364e42470187793fe7fd786698cea86e6c
Leeran <leeran7@gmail.com>
2022-06-09 09:38:34 -0400

    refactor(Issuance-type): Changed status type into a string, removed comments

--- src/types/Issuance.ts
+++ src/types/Issuance.ts
@@ -14,30 +14,7 @@ export type Issuance = {
   call_price: number;
   description: string;
   fee_paid: number;
-  status: number;
+  status: string;
   locked: number;
   asset_longname: string | null;
 };
-
-/*
-{
-			"tx_index": 1846055,
-			"tx_hash": "38b73acdb9d29459bb69b7cd7cc57994a22164fc98dc9cdf8d21feae05583efa",
-			"msg_index": 0,
-			"block_index": 717904,
-			"asset": "SPACECOASTER",
-			"quantity": 0,
-			"divisible": 0,
-			"source": "193HJNQrEhvP4nJf359uDVxobQBCzvFLtd",
-			"issuer": "193HJNQrEhvP4nJf359uDVxobQBCzvFLtd",
-			"transfer": 0,
-			"callable": 0,
-			"call_date": 0,
-			"call_price": 0.0,
-			"description": "",
-			"fee_paid": 50000000,
-			"locked": 0,
-			"status": "valid",
-			"asset_longname": null
-		},
-*/
